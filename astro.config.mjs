529e9c7011a06b6f643a4ed4408aa3b42a0819a1
MiauDona <donanferyt@gmail.com>
2024-02-14 14:41:57 +0100

    Fixed Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+      minify: false, // Minify HTML, CSS, and JS
+      compressHTML: false, // Further compress HTML
+      output: '[name].astro', // Customize output filenames
+    },
+  });
